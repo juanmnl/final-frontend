51f339616d04fa57e22c81eca3989ece23bec781
juanmnl <juanmnlcornejo@gmail.com>
2015-07-27 20:01:19 -0500

    Fix fizzbuzz comment

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -12,14 +12,14 @@ for (var i = 1; i <= 100; i++) {
 
 /*
  SIMPLER FizzBuzz
- whitout -> 'fizzbuzz' and a var declaration
+ better version
  */
 
-for (var n = 1; n <= 100; n++) {
+for (n = 1; n <= 100; n++) {
   var output = "";
-  if (n % 3 == 0)
+  if (n % 3 === 0)
     output += "Fizz";
-  if (n % 5 == 0)
+  if (n % 5 === 0)
     output += "Buzz";
   console.log(output || n);
 }
