e6e3361c8c43230eb4ea4a02bf180f2df27e8067
juanmnl <juanmnlcornejo@gmail.com>
2015-10-06 16:50:35 -0500

    Add this keyword implicit binding examples

---
+++ js/ch_1/examples/this.js
@@ -0,0 +1,85 @@
+// The this keyword is probably the most misunderstood aspect of JS
+
+// IMPLICIT BINDING
+// Look at the left of the Dot at call time
+
+//// EX.1
+var me = {
+  name: 'Juanmnl',
+  age : 31,
+  sayName: function() {
+    console.log(this.name);
+  }
+};
+
+me.sayName(); // this keyword referencing what is left of the DOT
+
+//// EX.2
+var SayNameMixin = function(obj) {
+  obj.sayName = function() {
+    console.log(this.name);
+  };
+};
+
+var me = {
+  name: 'Juanmnl',
+  age: 31
+};
+
+var you = {
+  name: 'Thom',
+  age: 2
+};
+
+SayNameMixin(me); // decorate object me with func sayName()
+SayNameMixin(you);// decorate object you with func sayName()
+
+me.sayName(); // >_ Juanmnl
+you.sayName();// >_ Thom
+
+//// EX.3
+
+var Person = function(name, age) { // Person 'class'
+  return {
+    name: name,
+    age: age,
+    sayName: function() {
+      console.log(this.name);
+    },
+    parent: {
+      name: "Janis",
+      age: 27,
+      sayName: function() {
+        console.log(this.name);
+      }
+    }
+  };
+};
+
+var thom = Person('Thom', 2);
+thom.sayName(); // >_ Thom
+thom.parent.sayName(); // >_ Janis
+
+
+// Explicit Binding
+
+// New Binding
+
+// Window Binding
+
+
+
+// WHERE IS THE FUNCTION INVOKED?
+
+
+
+// EXAMPLE OF INVOKING SOMETHING
+// define func
+var sayName = function(name) {
+  console.log("Hello, " + name);
+};
+
+// We don't know what name is, until it's invoked.
+
+// invoke func
+sayName('Juanmnl'); // >_ Hello Juanmnl
--- js/ch_1/exercises/chessboard.js
+++ js/ch_1/exercises/chessboard.js
@@ -3,7 +3,7 @@ var board = "";
 
 for (var y = 0; y < size; y++) {
   for (var x = 0; x < size; x++) {
-    if ((x + y) % 2 == 0)
+    if ((x + y) % 2 === 0)
       board += " ";
     else
       board += "#";
