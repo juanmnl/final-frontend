e88da10e0682df1e818a0de7ce971bbd4e1e16ca
juanmnl <juanmnlcornejo@gmail.com>
2018-01-04 11:08:27 -0500

    Add setTimeout to todo example

--- english/JS/todo.js
+++ english/JS/todo.js
@@ -1,55 +1,57 @@
-// V1 ---------------------
-// var todos = ['Buy milk 🥛'];
+setTimeout(function() {
+  // V1 ---------------------
+  // var todos = ['Buy milk 🥛'];
 
-// var input = prompt('What do you want to do?');
+  // var input = prompt('What do you want to do?');
 
-// while (input !== 'quit') {
-//   if (input === 'list') {
-//     console.log(todos);
-//   } else if (input === 'new') {
-//     var newTodo = prompt('Enter a todo...');
-//     todos.push(newTodo);
-//   }
-//   input = prompt('What do you want to do, now?');
-// }
+  // while (input !== 'quit') {
+  //   if (input === 'list') {
+  //     console.log(todos);
+  //   } else if (input === 'new') {
+  //     var newTodo = prompt('Enter a todo...');
+  //     todos.push(newTodo);
+  //   }
+  //   input = prompt('What do you want to do, now?');
+  // }
 
-// console.log('Ok, bye!');
+  // console.log('Ok, bye!');
 
-///////////////////////////
-// V2 ---------------------
-var todos = ['Buy milk 🥛', 'Buy groceries 🥦', 'Walk the dog 🐶'];
+  ///////////////////////////
+  // V2 ---------------------
+  var todos = ['Buy milk 🥛', 'Buy groceries 🥦', 'Walk the dog 🐶'];
 
-var input = prompt('What do you want to do?');
+  var input = prompt('What do you want to do?');
 
-while (input !== 'quit') {
-  if (input === 'list') {
-    printTodos();
-  } else if (input === 'new') {
-    newTodo();
-  } else if (input === 'delete') {
-    deleteTodo();
+  while (input !== 'quit') {
+    if (input === 'list') {
+      printTodos();
+    } else if (input === 'new') {
+      newTodo();
+    } else if (input === 'delete') {
+      deleteTodo();
+    }
+    input = prompt('What do you want to do, now?');
   }
-  input = prompt('What do you want to do, now?');
-}
 
-console.log('Ok, see you!');
+  console.log('Ok, see you!');
 
-function deleteTodo() {
-  var index = prompt('Which Todo would you like to remove?');
-  todos.splice(index, 1);
-  console.log('The item was removed from the list.');
-}
+  function deleteTodo() {
+    var index = prompt('Which Todo would you like to remove?');
+    todos.splice(index, 1);
+    console.log('The item was removed from the list.');
+  }
 
-function newTodo() {
-  var newTodo = prompt('Enter a todo...');
-  todos.push(newTodo);
-  console.log(newTodo + ' was added to the list.');
-}
+  function newTodo() {
+    var newTodo = prompt('Enter a todo...');
+    todos.push(newTodo);
+    console.log(newTodo + ' was added to the list.');
+  }
 
-function printTodos() {
-  console.log('**********');
-  todos.forEach(function(todo, i) {
-    console.log(i + ': ' + todo);
-  });
-  console.log('**********');
-}
+  function printTodos() {
+    console.log('**********');
+    todos.forEach(function(todo, i) {
+      console.log(i + ': ' + todo);
+    });
+    console.log('**********');
+  }
+}, 2000);
