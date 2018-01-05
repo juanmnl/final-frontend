4270d70c691e1e451a028b36209376b544a20da5
juanmnl <juanmnlcornejo@gmail.com>
2018-01-04 20:28:50 -0500

    Add DOM events basics

--- english/DOM/index.js
+++ english/DOM/index.js
@@ -1,1 +1,10 @@
 console.dir(document);
+
+var bpBtn = document.querySelector('#bipolar-btn');
+var bpP = document.getElementById('bipolar-p');
+
+bpBtn.addEventListener('click', changeText);
+
+function changeText() {
+  bpP.textContent = '>:| Hey! stop clicking on me!';
+}
